38d6a6c4ff820285f1cd8a341d834c15fb6abf8a
Victor Chen <victorchen2022@yahoo.com>
2020-12-02 22:16:26 -0500

    fixing things

--- src/main/java/dev/vchen04/warpbooks/Plugin.java
+++ src/main/java/dev/vchen04/warpbooks/Plugin.java
@@ -2,8 +2,9 @@ package dev.vchen04.warpbooks;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import dev.vchen04.warpbooks.commands.CreateWarpBook;
-import dev.vchen04.warpbooks.listeners.UseWarpBookListener;
+import dev.vchen04.warpbooks.listeners.UseBlankWarpBook;
+import dev.vchen04.warpbooks.listeners.UseWarpBook;
+import dev.vchen04.warpbooks.recipes.BlankWarpBook;
 
 public class Plugin extends JavaPlugin {
 
@@ -14,14 +15,15 @@ public class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getCommand("createwarpbook").setExecutor(new CreateWarpBook());
+        BlankWarpBook.createRecipe(this);
 
-        getServer().getPluginManager().registerEvents(new UseWarpBookListener(), this);
+        getServer().getPluginManager().registerEvents(new UseWarpBook(), this);
+        getServer().getPluginManager().registerEvents(new UseBlankWarpBook(), this);
     }
 
     @Override
     public void onDisable() {
-        
+
     }
 
 }
