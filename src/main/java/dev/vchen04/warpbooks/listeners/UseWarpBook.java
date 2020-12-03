158ed82bd28bec472d2dff3e9643fd4a1ea30473
Victor Chen <victorchen2022@yahoo.com>
2020-12-02 22:15:38 -0500

    rename UseWarpBookListener.java

---
+++ src/main/java/dev/vchen04/warpbooks/listeners/UseWarpBook.java
@@ -0,0 +1,67 @@
+package dev.vchen04.warpbooks.listeners;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Lectern;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+public class UseWarpBook implements Listener {
+
+    final Gson gson = new Gson();
+
+    private void teleportPlayer(Player player, ItemStack book) {
+
+        BookMeta bookMeta = (BookMeta) book.getItemMeta(); // Should fail if the item is not a book
+
+        JsonObject bookJsonData = gson.fromJson(bookMeta.getPage(2), JsonObject.class);
+
+        // Should fail if there is no position data
+        player.teleport(new Location(Bukkit.getWorld(bookJsonData.get("world").getAsString()),
+                bookJsonData.get("x").getAsDouble(), bookJsonData.get("y").getAsDouble(),
+                bookJsonData.get("z").getAsDouble(), bookJsonData.get("yaw").getAsFloat(),
+                bookJsonData.get("pitch").getAsFloat()));
+
+    }
+
+    @EventHandler
+    public void onPlayerInteractBook(PlayerInteractEvent event) {
+
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+
+        if ((action.equals(Action.RIGHT_CLICK_BLOCK) || action.equals(Action.RIGHT_CLICK_AIR)) && player.isSneaking()) {
+
+            try {
+
+                teleportPlayer(player, player.getInventory().getItemInMainHand());
+
+            } catch (Exception e) {
+
+            }
+
+            try {
+
+                Lectern lectern = (Lectern) event.getClickedBlock().getState();
+
+                teleportPlayer(player,
+                        lectern.getInventory().getItem(lectern.getInventory().first(Material.WRITTEN_BOOK)));
+
+            } catch (Exception e) {
+
+            }
+
+        }
+
+    }
+
+}
